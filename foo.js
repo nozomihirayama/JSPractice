614630592dcb73c393feca348990bae06fa2ad33
nozomi.hirayama <non0125.ccff@gmail.com>
2021-06-11 21:39:46 +0000

    IT大学(オブジェクト)

---
+++ foo.js
@@ -0,0 +1,20 @@
+let youtuber = {
+    list: {
+        business: {
+            youtuber: [
+                { name: "しまぶー", age: 20, techProgrammng(){} },
+                { name: "あっちゃん", age: 37, techHistory(){} }
+            ],
+            teach() {}
+        },
+        entertainment: {
+            youtuber: [{ name: "ヒカキン" }, { name: "はじめしゃちょー" }],
+            makeSmile() {}
+            
+        }
+    },
+    plan() {},
+    uploadVideo() {}
+};
+
+export default youtuber;
---
+++ index.js
@@ -0,0 +1,9 @@
+import youtuber from "./foo";
+
+
+youtuber.
+youtuber.list.business.youtuber[0].techProgrammng();
+
+youtuber.plan();
+
+youtuber.list.entertainment.makeSmile();
