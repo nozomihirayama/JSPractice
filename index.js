9d6cecb199f83356382abd0d713e132e1356d945
nozomihirayama <non0125.ccff@gmail.com>
2021-06-20 00:56:20 +0900

    お試し

--- index.js
+++ index.js
@@ -12,3 +12,22 @@ function 高階関数(コールバック関数) {
     // 処理
     コールバック関数();
 }
+
+function unfollow() {
+    console.log("フォローを外しました");
+}
+
+function cancelTweet() {
+    console.log("ツイートをキャンセルしました");
+}
+
+unfollow();
+cancelTweet();
+
+function confirmed(fn) {
+    if (window.confirm("実行しますか？")) {
+        fn();
+    }
+}
+
+confirmed(unfollow);
